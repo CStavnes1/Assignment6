9928e5d03a9423b4bfce00e77deca05b33e3d572
ChrisStavnes <ChrisStavnes@gmail.com>
2018-03-24 17:42:07 -0500

    homework working

--- assets/giphy.js
+++ assets/giphy.js
@@ -2,34 +2,64 @@
 "use strict"
 
 //array for buttons 
-
 let topics = ["Manchester United", "The Room", "Music", "Minnesota"];
 
-let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=M7AxqOfDelpmvVXbJ7uItpRQhwv0Qh30&limit=10";
+$(document).ready(function () {
 
-
-//function getAjax() {
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
     for (let i = 0; i < topics.length; i++) {
-    
-        $("#topic-buttons").append(`<button id="${topics[i]}">${topics[i]}`)
 
-        $().click(function() {
-            console.log(`value="${topics[i]}"`)
-        });
-
-
-
+        $("#topic-buttons").append(`<button class="button-select" data-topic="${topics[i]}">${topics[i]}`)
     }
 
 });
 
 
-//};
+
+//function to create new button
+let bunttonMaker = function () {
+    event.preventDefault();
+    $("#topic-buttons").empty();
+
+    let newbutton = $("#topic").val()
+
+    topics.push(newbutton)
+
+    for (let i = 0; i < topics.length; i++) {
+
+        $("#topic-buttons").append(`<button class="button-select" data-topic="${topics[i]}">${topics[i]}`)
+    }
+
+}
+
+$("#add-topic").on("click", bunttonMaker)
+
+
+//Ajax call and display giphs on click
+$("#topic-buttons").on("click", ".button-select", function () {
+    console.log($(event.target).data("topic"))
+
+    let topic = $(event.target).data("topic")
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=M7AxqOfDelpmvVXbJ7uItpRQhwv0Qh30&limit=10";
+
+    console.log(queryURL);
+
+
+    $.ajax({
+
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        for (let i = 0; i < response.data.length; i++) {
+
+            $("#giphs").append(`<div><img src="${response.data[i].images.original.url}"><br><span>${response.data[i].rating}</span></div>`)
+
+        };
+    });
+
+});
+
+
+
 
